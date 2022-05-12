694ed9a8a777042e4ab52871b6110a79112355b3
fritingo <fritingo.ai10@nycu.edu.tw>
2022-05-13 01:45:38 +0800

    p10

--- yolov4.py
+++ yolov4.py
@@ -1,6 +1,5 @@
 
 from turtle import forward
-from cv2 import pointPolygonTest
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -30,6 +29,21 @@ class SSP(nn.Module):
 
         return features
 
+# Conv operation
+class conv_op(nn.Module):
+    def __init__(self,in_filters,out_filters,kernel_size,stride=1):
+        super(conv_op,self).__init__()
+        
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_filters,out_filters,kernel_size,stride,padding=0,bias=False),
+            nn.BatchNorm2d(out_filters),
+            nn.LeakyReLU(0.1),
+        )
+    
+    def forward(self,x):
+        return self.conv(x)
+
+
 # 3 Conv
 class conv_3(nn.Module):
     def __init__(self,in_filters,mid_filters,out_filters):
@@ -50,6 +64,33 @@ class conv_3(nn.Module):
     def forward(self,x):
         return self.conv3(x)
 
+# 5 Conv
+class conv_5(nn.Module):
+    def __init__(self,in_filters,mid_filters,out_filters):
+        super().__init__()
+
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(in_filters,mid_filters,kernel_size=1,stride=1,padding=0,bias=False),
+            nn.BatchNorm2d(mid_filters),
+            nn.LeakyReLU(0.1),
+            nn.Conv2d(mid_filters,out_filters,kernel_size=3,stride=1,padding=1,bias=False),
+            nn.BatchNorm2d(out_filters),
+            nn.LeakyReLU(0.1),
+            nn.Conv2d(out_filters,mid_filters,kernel_size=1,stride=1,padding=0,bias=False),
+            nn.BatchNorm2d(mid_filters),
+            nn.LeakyReLU(0.1),
+            nn.Conv2d(mid_filters,out_filters,kernel_size=3,stride=1,padding=1,bias=False),
+            nn.BatchNorm2d(out_filters),
+            nn.LeakyReLU(0.1),
+            nn.Conv2d(out_filters,mid_filters,kernel_size=1,stride=1,padding=0,bias=False),
+            nn.BatchNorm2d(mid_filters),
+            nn.LeakyReLU(0.1),
+        )
+
+    def forward(self,x):
+        return self.conv5(x)
+
+
 # Upsampling
 class upsampling(nn.Module):
     def __init__(self,in_channels,out_channels):
@@ -64,9 +105,22 @@ class upsampling(nn.Module):
     def forward(self,x):
         return self.upsample(x)
 
+# Downsampling
+class downsampling(nn.Module):
+    def __init__(self,in_channels,out_channels):
+        super(downsampling,self).__init__()
+
+        self.downsampling = nn.Sequential(
+            nn.Conv2d(in_channels,out_channels,kernel_size=3,stride=2,padding=1,bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU(0.1),
+        )
+
+    def forward(self,x):
+        return self.downsampling(x)
 
 if __name__ == '__main__':
-    x = torch.randn(1,3,416,416)
+    x = torch.randn(5,3,416,416)
     backbone = CSPDarknet(3)
     
     x = backbone(x)
@@ -74,16 +128,48 @@ if __name__ == '__main__':
     # P1
     conv_x3 = conv_3(1024,512,1024)
     p1 = conv_x3(x[2])
-    print(p1.shape)
+    print(p1.shape) # batch_size x 512 x 13 x 13
     # SSP P2
     ssp = SSP()
     p2 = ssp(p1)
-    print(p2.shape)
+    print(p2.shape) # batch_size x 2048 x 13 x 13
     # P3
     conv_x3 = conv_3(2048,512,1024)
     p3 = conv_x3(p2)
-    print(p3.shape)
+    print(p3.shape) # batch_size x 512 x 13 x 13
     # P4
     upsample = upsampling(512,256)
     p4 = upsample(p3)
-    print(p4.shape)
+    print(p4.shape) # batch_size x 256 x 26 x 26
+
+    # P5
+    conv = conv_op(512,256,1,1)
+    p5 = conv(x[1])
+    print(p5.shape) # batch_size x 256 x 26 x 26
+
+    # P6
+    p6 = torch.cat((p4,p5),dim=1)
+    conv_x5 = conv_5(512,256,512)
+    p6 = conv_x5(p6)
+    print(p6.shape)
+
+    # P7
+    upsample = upsampling(256,128)
+    p7 = upsample(p6)
+    print(p7.shape)
+
+    # P8
+    conv = conv_op(256,128,1,1)
+    p8 = conv(x[0])
+    print(p8.shape)
+
+    # P9
+    p9 = torch.cat((p7,p8),dim=1)
+    conv_x5 = conv_5(256,128,256)
+    p9 = conv_x5(p9)
+    print(p9.shape)
+
+    # P10
+    downsample = downsampling(128,256)
+    p10 = downsample(p9)
+    print(p10.shape)
